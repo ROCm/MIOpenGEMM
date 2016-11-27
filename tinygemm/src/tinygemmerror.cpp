1df8a130b6fc6c930a25e855c865bd4c466c8b6a
James Newling <jnewling@idiap.com>
2016-11-26 18:41:23 -0600

    added --from tinygemm.-- to the error message of a tinygemm_error

--- tinygemm/src/tinygemmerror.cpp
+++ tinygemm/src/tinygemmerror.cpp
@@ -1,6 +1,6 @@
 #include "tinygemmerror.hpp"
 
 namespace tinygemm{
-tinygemm_error::tinygemm_error(const std::string& what_arg):std::runtime_error(what_arg){};
-tinygemm_error::tinygemm_error(const char* what_arg):std::runtime_error(what_arg){};
+tinygemm_error::tinygemm_error(const std::string& what_arg):std::runtime_error("from tinygemm. " + what_arg){};
+tinygemm_error::tinygemm_error(const char* what_arg):std::runtime_error("from tinygemm. " + std::string(what_arg)){};
 }
