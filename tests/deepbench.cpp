4916402310b8e81668d868e853c77e40cb0d8709
James Newling <james.newling@gmail.com>
2017-05-04 19:44:35 +0200

    preparing deepbench experi

--- tests/deepbench.cpp
+++ tests/deepbench.cpp
@@ -5,9 +5,9 @@ int main(){
 
   auto geometries = get_deepbench_geometries();
   float allotted_time = 200.00; 
-  unsigned n_iterations = 3;
+  unsigned n_iterations = 10;
   bool verbose = false;
-  std::vector<std::string> v_constraints = {""};
+  std::vector<std::string> v_constraints = {"A_WOS0__B_WOS0"};
   std::string basedir("/home/james/gemmpaper/data/apiless/dblarge/");
   run_find_experiments(geometries, allotted_time, verbose, v_constraints, n_iterations, basedir);
   
