995467f16f21a736f5fe4f8c06494d89f4cead6b
James Newling <james.newling@gmail.com>
2017-06-08 16:54:27 +0200

    comments

--- tests/gencache.cpp
+++ tests/gencache.cpp
@@ -1,31 +1,40 @@
 #include <tinygemm/iterexperiments.hpp>
 #include <tinygemm/tggeometryutil.hpp>
 
+/* example of how to generate cache entries */
 int main(){
+    
   
 
 
-  /* define a vector of geometries which you wish to generate cache entries for */
-  /*  m ,  n ,  k , lda ,ldb ,ldc , tA , tB  (here we assume tC = false isColMajor = true)  */ 
-  std::vector<std::tuple<unsigned, unsigned, unsigned, unsigned, unsigned, unsigned, bool, bool>> geotuples = {
-    /* miopen test configs (im2col+gemm) : */     
-    std::make_tuple(800, 64, 16, 16, 16, 800, true, false), /* tC0_tA1_tB0_colMaj1_m800_n64_k16_lda16_ldb16_ldc800_ws0_f32 */
-    std::make_tuple(16, 64, 800, 16, 800, 16, false, false),     
-    /* etc etc ... */
-    std::make_tuple(2,3,5,7,11,13, true, true),         
-  };
-  /* no workspace gemm kernels : */
-  unsigned ws_size = 0;
-  std::vector<tinygemm::TinyGemmGeometry> geometries = tinygemm::get_from_m_n_k_ldaABC_tA_tB(geotuples, ws_size);    
-  /* for `minimal' ldA, ldB and ldC, consider function get_from_m_n_k_tA_tB */
-  /* for different tC and isColMajor, consider using the TinyGemmGeometry constructor directly  */
+  /* (1)   define a vector of geometries which you wish to generate cache entries for. */
+  /* (1.1) This can be done with strings  */
+  std::vector<tinygemm::TinyGemmGeometry> geometries = {{"tC0_tA1_tB0_colMaj1_m1200_n1_k1_lda1_ldb1_ldc1200_ws0_f32", }};
+  
+  
+  if (false){
+    /* (1.2) Or it can be done `directly' */
+    geometries.emplace_back(
+    /*isColMajor tA tB tC lda ldb ldc m n k workspace_size floattype */
+        true, false, false, false, 10, 10, 10, 10, 10, 10, 0,'f');
+   
+    /* (1.3) or using a function which assumes tC is false and isColMajor is true  (m, n, k, lda, ldb, ldc, tA, tB), workspace_size */ 
+    auto more_geometries = tinygemm::get_from_m_n_k_ldaABC_tA_tB({std::make_tuple(800, 64, 16, 16, 16, 800, true, false)}, 0);
+    geometries.insert(geometries.end(), more_geometries.begin(), more_geometries.end());
     
-  /* define how long you want to search for (upper bound for each geometry) */
-  /* the maximum time */
+    /* (1.4) or using a function which also assumes `minimal' ldA, ldB, ldC */
+    more_geometries = tinygemm::get_from_m_n_k_tA_tB({std::make_tuple(20, 30, 40, false, false)}, 0);
+    geometries.insert(geometries.end(), more_geometries.begin(), more_geometries.end());
+  }
+
+
+    
+  /* (2)   define the search settings (upper bounds for each of the geometries) */
+  /* the maximum time to search, per geometry */
   float allotted_time = 120.00; 
-  /* the maximum number of restarts */
+  /* the maximum number of restarts during the search, per geometry */
   unsigned allotted_iterations = 30;
-  /* the number of times each kernel should be run during the search. (tradeoff : many runs means less exploration with more accurate estimates) */
+  /* the number of times each kernel should be run during the search. (tradeoff : many runs means less exploration with more accurate perf estimates) */
   unsigned n_runs_per_kernel = 3;
   /* the statistic for averaging over the n_runs_per_kernel runs. Max/Mean/Median (TODO. currently only Max supported) */  
   tinygemm::SummaryStat sumstat(tinygemm::Max);
@@ -34,18 +43,16 @@ int main(){
   
   bool verbose = false;
   /* path to a directory if you want a log of each of the searches (not nec, but useful for further analysis/debugging) */
-  std::string basedir(""); //"/home/james/tinygemmout/test1"
+  std::string basedir("/home/james/tinygemmout/test1");
     
-  /* the constraints on the kernel. "A_WOS0__B_WOS0" is for no workspace in GEMM. "A_WOS0__B_WOS0__C_ICE_1" is for deterministic  */
+  /* the constraints on the kernel. 
+   * "A_WOS0__B_WOS0" is for no workspace in GEMM 
+   * "A_WOS0__B_WOS0__C_ICE_1" is for no workspace and deterministic  */
   std::vector<std::string> v_constraints = {"A_WOS0__B_WOS0"};
 
 
   bool verbose_outer = true;
   std::string fn_outer("");
-  
   tinygemm::run_find_experiments(geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
-
-
-  
 
 }
