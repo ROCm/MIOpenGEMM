3745791b488c2f16361a030d7de816941b7588f6
James Newling <james.newling@gmail.com>
2017-06-28 17:21:34 +0200

    formatting programs

--- tests/experiment1.cpp
+++ tests/experiment1.cpp
@@ -1,96 +1,119 @@
 #include <miopengemm/iterexperiments.hpp>
 
-/* A set of experiments used while writing a report/paper */
-int main(){
+// A set of experiments used while writing a report/paper
+int main()
+{
 
-  bool write_to_dir = false;
-  std::string basedir ("");
-  
-  if (write_to_dir == true){
-    
-    /* change this to some directory on your system where the results can be written */ 
+  bool        write_to_dir = false;
+  std::string basedir("");
+
+  if (write_to_dir == true)
+  {
+
+    // change this to some directory on your system where the results can be written
     basedir = "/home/james/gemmpaper/data/apiless5/";
-  
-    if (basedir.back() != '/'){
+
+    if (basedir.back() != '/')
+    {
       basedir += "/";
     }
   }
 
-
-  float allotted_time = 2000.00; 
-  unsigned n_runs_per_kernel = 1;
+  float                   allotted_time     = 2000.00;
+  unsigned                n_runs_per_kernel = 1;
   MIOpenGEMM::SummaryStat sumstat(MIOpenGEMM::Max);
-  bool verbose = false;
-  bool verbose_outer = true;
-  std::string fn_outer("");
-  
-  
- 
-  unsigned n_iterations_smallgrowing = 0;      // at ~25 seconds each
-  unsigned n_iterations_square = 1;            // at ~6000 seconds each
-  unsigned n_iterations_smalldeep = 0;         // at ~250 seconds each
-  unsigned n_iterations_largedeep = 10;          // at ~1100 seconds each
-  unsigned n_iterations_problem_geometries = 0;  // 1000;
+  bool                    verbose       = false;
+  bool                    verbose_outer = true;
+  std::string             fn_outer("");
 
-  
-  std::cout << "\nSMALLGROWING EXPERIMENTS : HOW DOES PERFORMANCE SCALE AS K INCREASES  ?  " << std::endl;
+  unsigned n_iterations_smallgrowing       = 0;   // at ~25 seconds each
+  unsigned n_iterations_square             = 1;   // at ~6000 seconds each
+  unsigned n_iterations_smalldeep          = 0;   // at ~250 seconds each
+  unsigned n_iterations_largedeep          = 10;  // at ~1100 seconds each
+  unsigned n_iterations_problem_geometries = 0;   // 1000;
+
+  std::cout << "\nSMALLGROWING EXPERIMENTS : HOW DOES PERFORMANCE SCALE AS K INCREASES  ?  "
+            << std::endl;
   auto geometries = MIOpenGEMM::get_small_growing_geometries();
 
-  std::vector<std::string> v_constraints = {  
-  "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_MAC5_ICE8_NAW10",
-  "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_MAC5_ICE4_NAW10",
-  "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_MAC5_ICE2_NAW10",
-  "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_MAC5_ICE1_NAW10"};
+  std::vector<std::string> v_constraints = {
+    "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_"
+    "MAC5_ICE8_NAW10",
+    "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_"
+    "MAC5_ICE4_NAW10",
+    "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_"
+    "MAC5_ICE2_NAW10",
+    "A_MIC8_PAD1_PLU1_LIW1_MIW1_WOS0__B_MIC6_PAD1_PLU0_LIW0_MIW1_WOS0__C_UNR8_GAL1_PUN0_NAW16_UFO0_"
+    "MAC5_ICE1_NAW10"};
   std::string subdir(basedir + "smallgrowing/");
 
-  if (n_iterations_smallgrowing > 0){
-    MIOpenGEMM::FindParams find_params(allotted_time, n_iterations_smallgrowing, n_runs_per_kernel, sumstat);
-    MIOpenGEMM::run_find_experiments(geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
+  if (n_iterations_smallgrowing > 0)
+  {
+    MIOpenGEMM::FindParams find_params(
+      allotted_time, n_iterations_smallgrowing, n_runs_per_kernel, sumstat);
+    MIOpenGEMM::run_find_experiments(
+      geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
   }
-  
-  unsigned small_threshold = 1000*1000*200;
-  std::cout << "\nSMALLDEEP EXPERIMENTS : HOW DO WE DO ON THE SMALL DEEPBENCH PROBLEMS (WITH AND WITHOUT ICE ALLOWED) ? " << std::endl;
-  geometries = MIOpenGEMM::get_small_deepbench_geometries(small_threshold);
+
+  unsigned small_threshold = 1000 * 1000 * 200;
+  std::cout << "\nSMALLDEEP EXPERIMENTS : HOW DO WE DO ON THE SMALL DEEPBENCH PROBLEMS (WITH AND "
+               "WITHOUT ICE ALLOWED) ? "
+            << std::endl;
+  geometries    = MIOpenGEMM::get_small_deepbench_geometries(small_threshold);
   v_constraints = {"", "C_ICE1"};
   if (basedir != "")
     subdir = basedir + "smalldeep/";
-  
-  if (n_iterations_smalldeep > 0){
-    MIOpenGEMM::FindParams find_params(allotted_time, n_iterations_smalldeep, n_runs_per_kernel, sumstat);
-    MIOpenGEMM::run_find_experiments(geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
+
+  if (n_iterations_smalldeep > 0)
+  {
+    MIOpenGEMM::FindParams find_params(
+      allotted_time, n_iterations_smalldeep, n_runs_per_kernel, sumstat);
+    MIOpenGEMM::run_find_experiments(
+      geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
   }
 
-
-  std::cout << "\nLARGEDEEP EXPERIMENTS : HOW DO WE DO ON THE LARGE DEEPBENCH PROBLEMS (WITHOUT ICE ALLOWED) ?" << std::endl;
-  geometries = MIOpenGEMM::get_large_deepbench_geometries(small_threshold);
+  std::cout << "\nLARGEDEEP EXPERIMENTS : HOW DO WE DO ON THE LARGE DEEPBENCH PROBLEMS (WITHOUT "
+               "ICE ALLOWED) ?"
+            << std::endl;
+  geometries    = MIOpenGEMM::get_large_deepbench_geometries(small_threshold);
   v_constraints = {""};
   if (basedir != "")
     subdir = basedir + "largedeep/";
 
-  if (n_iterations_largedeep > 0){
-    MIOpenGEMM::FindParams find_params(allotted_time, n_iterations_largedeep, n_runs_per_kernel, sumstat);
-    MIOpenGEMM::run_find_experiments(geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
+  if (n_iterations_largedeep > 0)
+  {
+    MIOpenGEMM::FindParams find_params(
+      allotted_time, n_iterations_largedeep, n_runs_per_kernel, sumstat);
+    MIOpenGEMM::run_find_experiments(
+      geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
   }
 
-  std::cout << "\nSQUARE EXPERIMENTS : HOW DO WE DO ON THE STANDARD SQUARE PROBLEMS ? (~600s per iteration) " << std::endl;
-  geometries = MIOpenGEMM::get_square_geometries();
+  std::cout << "\nSQUARE EXPERIMENTS : HOW DO WE DO ON THE STANDARD SQUARE PROBLEMS ? (~600s per "
+               "iteration) "
+            << std::endl;
+  geometries    = MIOpenGEMM::get_square_geometries();
   v_constraints = {""};
   if (basedir != "")
     subdir = basedir + "square/";
-  if (n_iterations_square > 0){
-    MIOpenGEMM::FindParams find_params(allotted_time, n_iterations_square, n_runs_per_kernel, sumstat);
-    MIOpenGEMM::run_find_experiments(geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
+  if (n_iterations_square > 0)
+  {
+    MIOpenGEMM::FindParams find_params(
+      allotted_time, n_iterations_square, n_runs_per_kernel, sumstat);
+    MIOpenGEMM::run_find_experiments(
+      geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
   }
 
-
-  std::cout << "\nPROBLEM EXPERIMENTS : FOR GEOMETRIES WHICH ARE GIVING US A HARD TIME :-{|>" << std::endl;
-  geometries = MIOpenGEMM::get_problem_geometries();
+  std::cout << "\nPROBLEM EXPERIMENTS : FOR GEOMETRIES WHICH ARE GIVING US A HARD TIME :-{|>"
+            << std::endl;
+  geometries    = MIOpenGEMM::get_problem_geometries();
   v_constraints = {""};
   if (basedir != "")
     subdir = basedir + "problemgeoms/";
-  if (n_iterations_problem_geometries > 0){
-    MIOpenGEMM::FindParams find_params(allotted_time, n_iterations_problem_geometries, n_runs_per_kernel, sumstat);
-    MIOpenGEMM::run_find_experiments(geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
-  }  
+  if (n_iterations_problem_geometries > 0)
+  {
+    MIOpenGEMM::FindParams find_params(
+      allotted_time, n_iterations_problem_geometries, n_runs_per_kernel, sumstat);
+    MIOpenGEMM::run_find_experiments(
+      geometries, v_constraints, find_params, verbose, basedir, verbose_outer, fn_outer);
+  }
 }
- 
--- tests/smallgeometrytests.cpp
+++ tests/smallgeometrytests.cpp
@@ -1,60 +1,71 @@
-#include <miopengemm/miogemm.hpp>
-#include <miopengemm/basicfind.hpp>
 #include <string>
+#include <miopengemm/basicfind.hpp>
+#include <miopengemm/miogemm.hpp>
 
 template <typename TFloat>
-void geometrytest(bool isColMajor, bool tA, bool tB, bool tC, unsigned m, unsigned n, unsigned k){
+void geometrytest(bool isColMajor, bool tA, bool tB, bool tC, unsigned m, unsigned n, unsigned k)
+{
 
-  unsigned lda = ( tA == isColMajor ? k : m ) + 1;
-  unsigned ldb = ( tB == isColMajor ? n : k ) + 2;
-  unsigned ldc = ( tC == isColMajor ? n : m ) + 4;
-  
+  unsigned lda = (tA == isColMajor ? k : m) + 1;
+  unsigned ldb = (tB == isColMajor ? n : k) + 2;
+  unsigned ldc = (tC == isColMajor ? n : m) + 4;
+
   unsigned a_offset = 5;
   unsigned b_offset = 7;
   unsigned c_offset = 11;
-  
+
   unsigned tail_off_a = 13;
   unsigned tail_off_b = 17;
   unsigned tail_off_c = 19;
 
-  float allotted_time = 0.001;
-  unsigned allotted_iterations = 1;
-  unsigned n_runs_per_kernel = 1;
-  MIOpenGEMM::SummaryStat sumstat = MIOpenGEMM::Median;
-  
+  float                   allotted_time       = 0.001;
+  unsigned                allotted_iterations = 1;
+  unsigned                n_runs_per_kernel   = 1;
+  MIOpenGEMM::SummaryStat sumstat             = MIOpenGEMM::Median;
 
-  /* set verbose to true if you want output to terminal */
+  // set verbose to true if you want output to terminal
   bool verbose = false;
-  /* set logfile if you want output forked to file */
+  // set logfile if you want output forked to file
   std::string logfile("");
 
   std::string constraints_string = "A_WOS0__B_WOS0__C_ICE3";
-  
-  unsigned n_postfind_runs = 1;
-  bool do_cpu_test = true;
 
-  unsigned workspace_size = 3;
-  unsigned workspace_offset = 4;      
-  char floattype = sizeof(TFloat) == 4 ? 'f' : 'd';
-  MIOpenGEMM::Geometry gg (isColMajor, tA, tB, tC, lda, ldb, ldc, m, n, k, workspace_size, floattype);
-  MIOpenGEMM::Offsets offsets (a_offset, b_offset, c_offset, workspace_offset, tail_off_a, tail_off_b, tail_off_c);    
-  MIOpenGEMM::FindParams find_params(allotted_time, allotted_iterations, n_runs_per_kernel, sumstat);
-  MIOpenGEMM::basicfind(gg, offsets, find_params, verbose, logfile, constraints_string,  n_postfind_runs, do_cpu_test);
+  unsigned n_postfind_runs = 1;
+  bool     do_cpu_test     = true;
+
+  unsigned             workspace_size   = 3;
+  unsigned             workspace_offset = 4;
+  char                 floattype        = sizeof(TFloat) == 4 ? 'f' : 'd';
+  MIOpenGEMM::Geometry gg(
+    isColMajor, tA, tB, tC, lda, ldb, ldc, m, n, k, workspace_size, floattype);
+  MIOpenGEMM::Offsets offsets(
+    a_offset, b_offset, c_offset, workspace_offset, tail_off_a, tail_off_b, tail_off_c);
+  MIOpenGEMM::FindParams find_params(
+    allotted_time, allotted_iterations, n_runs_per_kernel, sumstat);
+  MIOpenGEMM::basicfind(
+    gg, offsets, find_params, verbose, logfile, constraints_string, n_postfind_runs, do_cpu_test);
 }
 
-int main(){
-  unsigned m = 55;
-  unsigned k = 118;
+int main()
+{
+  unsigned m     = 55;
+  unsigned k     = 118;
   unsigned testi = 0;
-  for (bool tC : {false, true}){
-    for (bool isColMajor : {false, true}){
-      for (bool tA : {false, true}){
-        for (bool tB : {false, true}){
-          for  (unsigned n : {m - 10, m + 10}){
+  for (bool tC : {false, true})
+  {
+    for (bool isColMajor : {false, true})
+    {
+      for (bool tA : {false, true})
+      {
+        for (bool tB : {false, true})
+        {
+          for (unsigned n : {m - 10, m + 10})
+          {
             testi += 1;
             k += 1;
             std::cout << "\ntest " << testi << "/32";
-            std::cout << "\nm=" << m << " n=" << n << " k=" << k << "\ntA=" << tA << " tB=" << tB << " tC=" << tC << " isColMajor=" << isColMajor << std::endl;
+            std::cout << "\nm=" << m << " n=" << n << " k=" << k << "\ntA=" << tA << " tB=" << tB
+                      << " tC=" << tC << " isColMajor=" << isColMajor << std::endl;
             std::cout << "<float>  ";
             geometrytest<float>(isColMajor, tA, tB, tC, m, n, k);
             std::cout << "<double> ";
