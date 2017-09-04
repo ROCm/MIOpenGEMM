e0a710faaa9aa911122030d5ddf52ea47eba5514
James Newling <james.newling@gmail.com>
2017-09-04 19:32:54 +0200

    experimenting

--- examples/find.cpp
+++ examples/find.cpp
@@ -14,7 +14,16 @@ int main()
 
 
   //Geometry gg("tC0_tA0_tB1_colMaj1_m3275_n4860_k64_lda3275_ldb4864_ldc3275_ws0_f32");
-  Geometry gg("tC0_tA0_tB1_colMaj1_m3275_n4860_k64_lda3275_ldb4864_ldc3275_ws0_f32");
+  //Geometry gg("tC0_tA1_tB0_colMaj1_m3072_n48000_k1024_lda1024_ldb1024_ldc3072_ws52594944_f32");
+
+
+  Geometry gg("tC0_tA1_tB0_colMaj1_m3072_n48000_k1024_lda1024_ldb1024_ldc3072_ws0_f32");
+  
+  
+  //6249.987
+  
+  //tC=0 tA=1 tB=0 colMaj=1 m=3072   n=48000  k=1024   lda=1024   ldb=1024   ldc=3072   ws=52594944 f=32  time[ms]:54.058416   gflops:5586.36
+
 
   CLHint         devhint({"Advanced Micro Devices", "gfx803"});
   Offsets        offsets = get_zero_offsets();
