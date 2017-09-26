07409b8f6aebd1e4b198f0dd5819c88690848b23
James Newling <james.newling@gmail.com>
2017-09-26 16:48:42 +0200

    remove print in miog find

--- miopengemm/src/miogemm.cpp
+++ miopengemm/src/miogemm.cpp
@@ -118,13 +118,9 @@ Solution find(float            allotted_time,
               bool             with_warnings)
 {
 
-  std::cout << tgg.get_string() << std::endl;
   (void)with_warnings;
   bool   c_is_const    = true;
   cl_mem workspace_gpu = nullptr;
-  // verbose = true;
-  
-  std::cout << tgg.geometry();
   
   Ver::E         e_ver              = verbose ? Ver::E::TERMINAL : Ver::E::SILENT;
   std::string    constraints_string = enforce_determinism ? "C__ICE1" : "";
